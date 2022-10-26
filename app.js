596c3eab5f2c49ce4df2dbaac91840d4bb21f16f
sarah 3 <109850449+kido6596@users.noreply.github.com>
2022-10-26 09:34:03 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
 let circleTurn
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, { once: true })
-});
+startGame()
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+    setBoardHoverClass()
+}
 
 function handleClick(e) {
     const cell = e.target
@@ -14,8 +21,24 @@ function handleClick(e) {
     // check for win
     // check for draw
     // switch turns
+    swapTurns()
+    setBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
     cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if(circleTurn) {
+        board.classList.add(CIRCLE_CLAS)
+    } else{
+        board.classList.add(X_CLASS)
+    }
 }
